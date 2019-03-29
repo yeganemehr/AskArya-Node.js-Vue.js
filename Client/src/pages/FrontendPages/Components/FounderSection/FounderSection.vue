552cb0707dbb2b1cabe4058e50cba27063f8532e
Arya Doroudian <arya.doroudian1@gmail.com>
2019-03-29 16:38:09 +0430

    1

--- Client/src/pages/FrontendPages/Components/FounderSection/FounderSection.vue
+++ Client/src/pages/FrontendPages/Components/FounderSection/FounderSection.vue
@@ -2,42 +2,44 @@
   <div class="container pb-5">
     <h1 class="pb-5 founder-header text-danger text-right">بنیانگذار ما</h1>
     <div class="row">
-      <div class="col-md-8 text-right">
+      <div class="col-md-7 text-right">
         <h2 class="subheading2 text-info">کاپیتان این پرواز چه کسی است؟</h2>
-        <p>
+        <p class="CustomParagraph">
           با سلام، بنیان گذار اسک آریا آقای آریا درودیان هستند، اکثر دوران زندگی و تحصیل خود را در لندن گذرانده است. وی دو سال اخیر برای ادامه زندگی و اتمام دوره خلبانی خود به ایران سفر کرد و ساکن شد. آریا درودیان در اصل یک نیتیو دو زبانه انگلیسی فارسی است، همچنین در زمینه زبان فرانسه نیز تبحر دارد.
           <br>
           <br>وی فارغ التحصیل رشته امنیت شبکه از دانشگاه Westminster University لندن است. برای گذراندن دوره خلبانی به ایران سفر کرد و در حال حاظر دارای مدرک EASA ATPL و ICAO CPL میباشد.
           <br>
           <br>آریا درودیان همواره مشتاق سفر و به دست آوردن تجارب جدید است، او بنا به این علاقه به بیشتر کشور های دنیا سفر کرده و با زبان های رسمی مختلف دنیا آشناست.
         </p>
-        <div class="Education py-4">
-          <hr class="hr-1">
-          <h2 class="subheading2 text-info">تحصیلات</h2>
-          <p>
-            <span>خلبان مسافربری (ATPL) -</span>
-            <small>دانشکده خلبانی آکسفورد - لندن</small>
-          </p>
-          <p>
-            <span>لیسانس مهندسی امنیت شبکه کامپیوتر -</span>
-            <small>دانشگاه وستمینیستر (Westminister University) - لندن</small>
-          </p>
-          <p>
-            <span>فوق دیپلم کسب و کار، زبان فارسی، ریاضیات و حسابداری -</span>
-            <small>پیش دانشگاهی ویلیام موریس (William Morris Sixth Form) - لندن</small>
-          </p>
-          <p>
-            <span>دیپلم ریاضیات، ریاضیات (آمار)، علوم پایه، انگلیسی، فرانسوی، فارسی، کامپیوتر، و تاریخ -</span>
-            <small>دبیرستان هلند پارک (Holland Park School) - لندن</small>
-          </p>
-        </div>
       </div>
+
       <div class="col-md-4 py-4 text-center">
         <img
           class="rounded-circle img-fluid"
           src="img/frontend/aryapilot-askarya.png"
           alt="Picture of our fearless leader 2"
         >
+      </div>
+
+      <div class="Education py-4 text-right">
+        <hr class="hr-1">
+        <h2 class="subheading2 text-info">تحصیلات</h2>
+        <p class="CustomParagraph">
+          <span>خلبان مسافربری (ATPL) -</span>
+          <small>دانشکده خلبانی آکسفورد - لندن</small>
+        </p>
+        <p class="CustomParagraph">
+          <span>لیسانس مهندسی امنیت شبکه کامپیوتر -</span>
+          <small>دانشگاه وستمینیستر (Westminister University) - لندن</small>
+        </p>
+        <p class="CustomParagraph">
+          <span>فوق دیپلم کسب و کار، زبان فارسی، ریاضیات و حسابداری -</span>
+          <small>پیش دانشگاهی ویلیام موریس (William Morris Sixth Form) - لندن</small>
+        </p>
+        <p class="CustomParagraph">
+          <span>دیپلم ریاضیات، ریاضیات (آمار)، علوم پایه، انگلیسی، فرانسوی، فارسی، کامپیوتر، و تاریخ -</span>
+          <small>دبیرستان هلند پارک (Holland Park School) - لندن</small>
+        </p>
       </div>
     </div>
   </div>
@@ -54,5 +56,9 @@
 .subheader-founder {
   font-size: 1rem;
   color: rgb(197, 197, 197);
+}
+
+.CustomParagraph {
+  font-size: 1rem;
 }
 </style>
