96d46bfc1988e4feb8b95b8ad8764df25d407d63
Arya Doroudian <arya.doroudian1@gmail.com>
2019-04-22 13:49:39 +0430

    2.0.9.4

--- Client/src/pages/FrontendPages/Components/TopCourses/Course.vue
+++ Client/src/pages/FrontendPages/Components/TopCourses/Course.vue
@@ -1,268 +1,268 @@
 <template>
-	<div>
-		<router-link :to="'courses/' + slug">
-			<div class="hover m-widget7 m-widget7--skin-dark widget-3">
-				<div class="m-widget7__courseimage">
-					<img class="m-widget7__courseimage" :src="thumb">
-				</div>
-				<div class="course__name">
-					<h4 class="text-center font-weight-bold course-title pt-4">{{ title }}</h4>
-				</div>
-				<div class="m-widget7__course__info d-flex justify-content-between text-center pt-2">
-					<div>
-						<p class="courseTime">
-							{{ minutes }}
-							<br>
-							<span class="courseTimeSpan">دقیقه صوت و تصویر</span>
-						</p>
-					</div>
-
-					<div>
-						<p class="unitsAvailable">
-							{{ episodes }}
-							<br>
-							<span class="unitsAvailableSpan">درس گفتار</span>
-						</p>
-					</div>
-				</div>
-
-				<hr class="fadeline">
-				<div class="text-center">
-					<div>
-						<p class="pricehighlight">
-							<i class="tim-icons icon-money-coins pl-2"></i>
-							{{ formatedPrice }}
-						</p>
-					</div>
-				</div>
-				<hr class="fadeline2">
-				<div class="buyCourse text-center">
-					<p class="buyCourseTitle pb-3">تهیه دوره‌</p>
-				</div>
-			</div>
-		</router-link>
-	</div>
+  <div>
+    <router-link :to="'courses/' + slug">
+      <div class="CourseCard hover">
+        <div>
+          <img class="CourseCard__courseimage" :src="thumb">
+        </div>
+        <div class="course__name">
+          <h4 class="text-center font-weight-bold course-title pt-4">{{ title }}</h4>
+        </div>
+        <div class="CourseCard__course__info d-flex justify-content-between text-center pt-2">
+          <div>
+            <p class="courseTime">
+              {{ minutes }}
+              <br>
+              <span class="courseTimeSpan">دقیقه صوت و تصویر</span>
+            </p>
+          </div>
+          <div>
+            <p class="unitsAvailable">
+              {{ episodes }}
+              <br>
+              <span class="unitsAvailableSpan">درس گفتار</span>
+            </p>
+          </div>
+        </div>
+        <hr class="fadeline">
+        <div class="text-center">
+          <div>
+            <p class="pricehighlight">
+              <i class="tim-icons icon-money-coins pl-2"></i>
+              {{ formatedPrice }}
+            </p>
+          </div>
+        </div>
+        <hr class="fadeline2">
+        <div class="buyCourse text-center">
+          <p class="buyCourseTitle pb-3">تهیه دوره‌</p>
+        </div>
+      </div>
+    </router-link>
+  </div>
 </template>
 
 <script>
 import moment from 'moment';
 
 export default {
-	props: [
-		'title',
-		'thumb',
-		'time',
-		'slug',
-		'type',
-		'price',
-		'updatedAt',
-		'user',
-		'episodes'
-	],
-	computed: {
-		date() {
-			return moment(this.updatedAt).format('MM/DD/YYYY');
-		},
-		formatedPrice() {
-			if (this.type == 'free') {
-				return 'رایگان';
-			}
-			return (
-				new Intl.NumberFormat().format(
-					parseInt(this.price.replace(',', ''), 10)
-				) + ' تومان '
-			);
-		},
-		minutes() {
-			const time = this.time.split(":", 3);
-			let minutes = 0;
-			for (let i = 0; i < time.length; i++) {
-				minutes += Math.ceil(parseInt(time[i]) * Math.pow(60, time.length - i - 2));
-			}
-			return minutes;
-		}
-	}
+  props: [
+    'title',
+    'thumb',
+    'time',
+    'slug',
+    'type',
+    'price',
+    'updatedAt',
+    'user',
+    'episodes'
+  ],
+  computed: {
+    date() {
+      return moment(this.updatedAt).format('MM/DD/YYYY');
+    },
+    formatedPrice() {
+      if (this.type == 'free') {
+        return 'رایگان';
+      }
+      return (
+        new Intl.NumberFormat().format(
+          parseInt(this.price.replace(',', ''), 10)
+        ) + ' تومان '
+      );
+    },
+    minutes() {
+      const time = this.time.split(':', 3);
+      let minutes = 0;
+      for (let i = 0; i < time.length; i++) {
+        minutes += Math.ceil(
+          parseInt(time[i]) * Math.pow(60, time.length - i - 2)
+        );
+      }
+      return minutes;
+    }
+  }
 };
 </script>
 
 
 <style lang="scss" scoped>
 .outline {
-	border-radius: 20px;
+  border-radius: 20px;
 }
 
 .pricehighlight {
-	font-size: 0.9rem;
-	color: #fff;
-	font-weight: 600;
+  font-size: 1rem;
+  color: #fff;
+  font-weight: 600;
 }
 
 .fadeline {
-	height: 1px;
-	background-image: linear-gradient(
-		90deg,
-		rgba(255, 0, 0, 0),
-		rgba(145, 161, 145, 0.705) 50%,
-		rgba(255, 0, 0, 0) 100%
-	);
+  height: 1px;
+  background-image: linear-gradient(
+    90deg,
+    rgba(255, 0, 0, 0),
+    rgba(145, 161, 145, 0.705) 50%,
+    rgba(255, 0, 0, 0) 100%
+  );
 }
 
 .fadeline2 {
-	height: 1px;
-	background-image: linear-gradient(
-		90deg,
-		rgba(255, 0, 0, 0),
-		rgba(181, 216, 181, 0.226) 50%,
-		rgba(255, 0, 0, 0) 100%
-	);
+  height: 1px;
+  background-image: linear-gradient(
+    90deg,
+    rgba(255, 0, 0, 0),
+    rgba(181, 216, 181, 0.123) 50%,
+    rgba(255, 0, 0, 0) 100%
+  );
 }
 
 .course-info {
-	border-top: 3px solid rgba(110, 85, 85, 0.473);
+  border-top: 3px solid rgba(110, 85, 85, 0.473);
 }
 
-.widget-3 {
-	background-color: #32325d;
-	// padding: 20px 0;
-	/* border: 1px solid #1d1d46; */
-	border-radius: 25px;
+.CourseCard {
+  background-color: #32325d;
+  border-radius: 35px;
 }
 
 .hover:hover,
 .hover:focus {
-	-webkit-box-shadow: 0 10px 30px -5px rgba(97, 97, 97, 0.144);
-	box-shadow: 0 15px 35px -5px rgba(7, 78, 45, 0.288);
-	transition: transform 0.6s;
-	-webkit-transform: translateY(-6px);
-	transform: translateY(-6px);
-	cursor: pointer;
+  -webkit-box-shadow: 0 10px 30px -5px rgba(97, 97, 97, 0.144);
+  box-shadow: 0 15px 35px -5px rgba(7, 78, 45, 0.288);
+  transition: transform 0.6s;
+  -webkit-transform: translateY(-6px);
+  transform: translateY(-6px);
+  cursor: pointer;
 }
 
-.m-widget7__title {
-	text-align: center;
+.CourseCard__title {
+  text-align: center;
 }
 
 .course-title {
-	font-size: 1.1rem;
-	color: #f1f1f1;
+  font-size: 1.2rem;
+  color: #f1f1f1;
 }
 
-.m-widget7 .m-widget7__courseimage {
-	border-radius: 15px;
+.CourseCard .CourseCard__courseimage {
+  border-radius: 30px 30px 0 0;
 }
 
-.m-widget7 .m-widget7__description {
-	text-align: center;
-	margin-top: 4rem;
-	padding: 0 30px;
-	font-size: 1.1rem;
+.CourseCard .CourseCard__description {
+  text-align: center;
+  margin-top: 4rem;
+  padding: 0 30px;
+  font-size: 1.1rem;
 }
 
-.m-widget7 .m-widget7__user {
-	margin: 0 auto;
-	display: table;
-	margin-top: 0.35rem;
-	margin-bottom: 1rem;
-	margin-left: 1.9rem;
+.CourseCard .CourseCard__user {
+  margin: 0 auto;
+  display: table;
+  margin-top: 0.35rem;
+  margin-bottom: 1rem;
+  margin-left: 1.9rem;
 }
 
-.m-widget7 .m-widget7__user .m-widget7__user-img .m-widget7__img {
-	width: 2.3rem;
-	border-radius: 50%;
-	margin-top: 1.14rem;
+.CourseCard .CourseCard__user .CourseCard__user-img .CourseCard__img {
+  width: 2.3rem;
+  border-radius: 50%;
+  margin-top: 1.14rem;
 }
 
-.m-widget7 .m-widget7__user .m-widget7__info {
-	display: table-cell;
+.CourseCard .CourseCard__user .CourseCard__info {
+  display: table-cell;
 }
 
-.m-widget7 .m-widget7__user .m-widget7__info .m-widget7__username {
-	margin-top: 0.71rem;
-	margin-left: 0.71rem;
-	font-size: 0.8rem;
-	font-weight: 500;
+.CourseCard .CourseCard__user .CourseCard__info .CourseCard__username {
+  margin-top: 0.71rem;
+  margin-left: 0.71rem;
+  font-size: 0.8rem;
+  font-weight: 500;
 }
 
-.m-widget7 .m-widget7__user .m-widget7__info .m-widget7__time {
-	margin-left: 0.71rem;
-	margin-top: 0.71rem;
-	font-size: 0.75rem;
+.CourseCard .CourseCard__user .CourseCard__info .CourseCard__time {
+  margin-left: 0.71rem;
+  margin-top: 0.71rem;
+  font-size: 0.75rem;
 }
 
-.m-widget7 .m-widget7__button {
-	text-align: center;
-	margin-bottom: 1.85rem;
+.CourseCard .CourseCard__button {
+  text-align: center;
+  margin-bottom: 1.85rem;
 }
 
-.m-widget7__course__info {
-	margin: 0 15%;
+.CourseCard__course__info {
+  margin: 0 15%;
 }
 
 .courseTime {
-	color: rgb(219, 219, 219);
-	font-size: 1.2rem;
-	font-weight: 600;
+  color: rgb(219, 219, 219);
+  padding-right: 1.2rem;
+  font-size: 1.2rem;
+  font-weight: 600;
 }
 .courseTimeSpan {
-	font-size: 0.73rem;
-	color: #adadad;
+  font-size: 0.73rem;
+  color: #adadad;
 }
 
 .unitsAvailable {
-	color: rgb(219, 219, 219);
-	font-size: 1.2rem;
-	font-weight: 600;
+  color: rgb(219, 219, 219);
+  padding-left: 1.2rem;
+  font-size: 1.2rem;
+  font-weight: 600;
 }
 .unitsAvailableSpan {
-	font-size: 0.73rem;
-	color: #adadad;
+  font-size: 0.73rem;
+  color: #adadad;
 }
 
-.m-widget7 .m-widget7__description {
-	color: #575962;
+.CourseCard .CourseCard__description {
+  color: #575962;
 }
 
-.m-widget7 .m-widget7__info .m-widget7__username {
-	color: #575962;
+.CourseCard .CourseCard__info .CourseCard__username {
+  color: #575962;
 }
 
-.m-widget7 .m-widget7__info .m-widget7__time {
-	color: #9c9c9c !important;
+.CourseCard .CourseCard__info .CourseCard__time {
+  color: #9c9c9c !important;
 }
 
-.m-widget7.m-widget7--skin-dark .m-widget7__description {
-	color: #fff;
+.CourseCard.CourseCard--skin-dark .CourseCard__description {
+  color: #fff;
 }
 
-.m-widget7.m-widget7--skin-dark .m-widget7__info .m-widget7__username {
-	color: #fff;
+.CourseCard.CourseCard--skin-dark .CourseCard__info .CourseCard__username {
+  color: #fff;
 }
 
-.m-widget7.m-widget7--skin-dark .m-widget7__info .m-widget7__time {
-	color: #fff;
+.CourseCard.CourseCard--skin-dark .CourseCard__info .CourseCard__time {
+  color: #fff;
 }
 
 .buyCourseTitle {
-	font-size: 1.1rem;
-	color: rgb(119, 119, 247);
+  font-size: 1.1rem;
+  color: rgb(92, 92, 252);
 }
 
-.m-widget7:hover {
-	.buyCourseTitle {
-		color: rgb(81, 202, 206);
-		font-weight: 600;
-	}
-	.course-title {
-		color: #f3ef1f;
-	}
-	.courseTime {
-		color: #f3ef1f;
-	}
-	.unitsAvailable {
-		color: #f3ef1f;
-	}
-	.pricehighlight {
-		color: #f3ef1f;
-	}
+.CourseCard:hover {
+  .buyCourseTitle {
+    color: rgb(81, 202, 206);
+    font-weight: 600;
+  }
+  .course-title {
+    color: #f3ef1f;
+  }
+  .courseTime {
+    color: #f3ef1f;
+  }
+  .unitsAvailable {
+    color: #f3ef1f;
+  }
+  .pricehighlight {
+    color: #f3ef1f;
+  }
 }
 </style>
--- Server/app/http/controllers/api/v1/homeController.js
+++ Server/app/http/controllers/api/v1/homeController.js
@@ -1,140 +1,140 @@
-const controller = require("app/http/controllers/api/controller");
-const Payment = require("app/models/payment");
-const Course = require("app/models/course");
-const CourseController = require("./courseController");
+const controller = require('app/http/controllers/api/controller');
+const Payment = require('app/models/payment');
+const Course = require('app/models/course');
+const CourseController = require('./courseController');
 
 class homeController extends controller {
-	async index(req, res) {
-		const topCourses = Course.find()
-			.populate("user", "id name")
-			.populate("categories", "name slug")
-			.populate("episodesCount")
-			.limit(4)
-			.sort({
-				viewCount: "desc"
-			})
-			.exec();
-		const user = req.user
-			? req.user
-					.populate({
-						path: "roles",
-						select: "name label permissions",
-						populate: [
-							{
-								path: "permissions"
-							}
-						]
-					})
-					.execPopulate()
-			: undefined;
-		// const topBlogPosts = Posts.find().limit(8).sort({viewCount: "desc"}).exec();
-		const topPosts = undefined;
-		const results = await Promise.all([topCourses, user, topPosts]);
-		return res.json({
-			status: "success",
-			topCourses: results[0].map(CourseController.filterCourse),
-			user: results[1] ? this.filterUserData(results[1]) : undefined,
-			topPosts: results[2]
-		});
-	}
-	async user(req, res) {
-		let user = await req.user
-			.populate({
-				path: "roles",
-				select: "name label permissions",
-				populate: [
-					{
-						path: "permissions"
-					}
-				]
-			})
-			.execPopulate();
+  async index(req, res) {
+    const topCourses = Course.find()
+      .populate('user', 'id name')
+      .populate('categories', 'name slug')
+      .populate('episodesCount')
+      .limit(3)
+      .sort({
+        viewCount: 'desc'
+      })
+      .exec();
+    const user = req.user
+      ? req.user
+          .populate({
+            path: 'roles',
+            select: 'name label permissions',
+            populate: [
+              {
+                path: 'permissions'
+              }
+            ]
+          })
+          .execPopulate()
+      : undefined;
+    // const topBlogPosts = Posts.find().limit(8).sort({viewCount: "desc"}).exec();
+    const topPosts = undefined;
+    const results = await Promise.all([topCourses, user, topPosts]);
+    return res.json({
+      status: 'success',
+      topCourses: results[0].map(CourseController.filterCourse),
+      user: results[1] ? this.filterUserData(results[1]) : undefined,
+      topPosts: results[2]
+    });
+  }
+  async user(req, res) {
+    let user = await req.user
+      .populate({
+        path: 'roles',
+        select: 'name label permissions',
+        populate: [
+          {
+            path: 'permissions'
+          }
+        ]
+      })
+      .execPopulate();
 
-		return res.json({
-			data: this.filterUserData(user),
-			status: "success"
-		});
-	}
+    return res.json({
+      data: this.filterUserData(user),
+      status: 'success'
+    });
+  }
 
-	async history(req, res) {
-		try {
-			let page = req.query.page || 1;
-			let payments = await Payment.paginate(
-				{
-					user: req.user.id
-				},
-				{
-					page,
-					sort: {
-						createdAt: -1
-					},
-					limit: 20,
-					populate: [
-						{
-							path: "course"
-						},
-						{
-							path: "user",
-							select: "name email"
-						}
-					]
-				}
-			);
+  async history(req, res) {
+    try {
+      let page = req.query.page || 1;
+      let payments = await Payment.paginate(
+        {
+          user: req.user.id
+        },
+        {
+          page,
+          sort: {
+            createdAt: -1
+          },
+          limit: 20,
+          populate: [
+            {
+              path: 'course'
+            },
+            {
+              path: 'user',
+              select: 'name email'
+            }
+          ]
+        }
+      );
 
-			res.json({
-				data: this.filterPaymentData(payments),
-				status: "success"
-			});
-		} catch (err) {
-			this.failed(err.message, res);
-		}
-	}
+      res.json({
+        data: this.filterPaymentData(payments),
+        status: 'success'
+      });
+    } catch (err) {
+      this.failed(err.message, res);
+    }
+  }
 
-	filterPaymentData(payments) {
-		return {
-			...payments,
-			docs: payments.docs.map(pay => {
-				return {
-					payment: pay.payment,
-					resnumber: pay.resnumber,
-					price: pay.price,
-					user: {
-						name: pay.user.name,
-						email: pay.user.email
-					}
-				};
-			})
-		};
-	}
+  filterPaymentData(payments) {
+    return {
+      ...payments,
+      docs: payments.docs.map(pay => {
+        return {
+          payment: pay.payment,
+          resnumber: pay.resnumber,
+          price: pay.price,
+          user: {
+            name: pay.user.name,
+            email: pay.user.email
+          }
+        };
+      })
+    };
+  }
 
-	filterUserData(user) {
-		return {
-			id: user.id,
-			active: user.active,
-			admin: user.admin,
-			name: user.name,
-			email: user.email,
-			createdAt: user.createdAt,
-			vipTime: user.vipTime,
-			vipType: user.vipType,
-			lang: user.lang,
-			avatar: user.avatar,
-			roles: user.roles
-				? user.roles.map(role => {
-						return {
-							name: role.name,
-							label: role.label,
-							permissions: role.permissions.map(per => {
-								return {
-									name: per.name,
-									label: per.label
-								};
-							})
-						};
-					})
-				: undefined
-		};
-	}
+  filterUserData(user) {
+    return {
+      id: user.id,
+      active: user.active,
+      admin: user.admin,
+      name: user.name,
+      email: user.email,
+      createdAt: user.createdAt,
+      vipTime: user.vipTime,
+      vipType: user.vipType,
+      lang: user.lang,
+      avatar: user.avatar,
+      roles: user.roles
+        ? user.roles.map(role => {
+            return {
+              name: role.name,
+              label: role.label,
+              permissions: role.permissions.map(per => {
+                return {
+                  name: per.name,
+                  label: per.label
+                };
+              })
+            };
+          })
+        : undefined
+    };
+  }
 }
 
 module.exports = new homeController();
