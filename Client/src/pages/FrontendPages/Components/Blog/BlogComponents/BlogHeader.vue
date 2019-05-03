f31a35ec5205081ac7617349e133dc9e641d29b7
Arya Doroudian <arya.doroudian1@gmail.com>
2019-05-03 19:41:55 +0430

    2.1.0.9

--- Client/src/pages/FrontendPages/Components/Blog/BlogComponents/BlogHeader.vue
+++ Client/src/pages/FrontendPages/Components/Blog/BlogComponents/BlogHeader.vue
@@ -1,19 +1,16 @@
 <template>
-  <div class="col-md-12">
-    <h1 class="text-center pt-5 text-danger text-ltr">{{ title }}</h1>
+  <div class="col-md-12 pt-5">
+    <h1 class="text-center text-danger text-ltr">{{ title }}</h1>
   </div>
 </template>
 
 <script>
 export default {
-  props: [ 'title' ],
-}
+  props: ['title']
+};
 </script>
 
 <style lang="scss" scoped>
-.card {
-  border-radius: 5px;
-}
 h1 {
   font-weight: 600;
 }
