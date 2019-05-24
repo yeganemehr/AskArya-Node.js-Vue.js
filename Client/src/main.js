f5caadd0cbe392cf39b44b19296202db58b7d185
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2019-05-24 14:19:30 +0430

    Fix #88

--- Client/src/main.js
+++ Client/src/main.js
@@ -33,44 +33,6 @@ function start(data) {
     data
   });
 }
-const defaultBlogPosts = [
-  {
-    id: '5c44hddb45f5f8340747961e4',
-    slug: 'introduction-into-ielts',
-    category: 'IELTS',
-    thumb: '/img/frontend/intro-ielts-askarya.png',
-    title: 'An introduction into IELTS.',
-    createdAt: '2018-05-28',
-    viewCount: 47571
-  },
-  {
-    id: '5c44hddb45f53w40747945e4',
-    slug: 'lets-talk-about-numbers',
-    category: 'General',
-    thumb: '/img/frontend/numbers-askarya.png',
-    title: "Let's talk about numbers.",
-    createdAt: '2018-06-20',
-    viewCount: 53485
-  },
-  {
-    id: '5c44hddb45f53w40747961e4',
-    slug: 'expressions-of-sympathy',
-    category: 'General',
-    thumb: '/img/frontend/sympathy-askarya.png',
-    title: 'Expressions of sympathy',
-    createdAt: '2018-06-29',
-    viewCount: 40862
-  },
-  {
-    id: '5c44hddb45f53w40747961e4',
-    slug: 'easily-confused-words-in-english',
-    category: 'General',
-    thumb: '/img/frontend/32confusingwords-askarya.png',
-    title: 'Easily Confused Words in English.',
-    createdAt: '2018-07-06',
-    viewCount: 48802
-  }
-];
 backend
   .get('/home')
   .then(response => {
