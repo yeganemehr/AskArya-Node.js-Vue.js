be57a922c7d20cd2f2de3e2001353013c5238c76
arya107 <arya.doroudian1@gmail.com>
2019-08-07 21:08:28 +0430

    2.2.7.3

--- Client/src/pages/FrontendPages/Components/Blog/BlogCard.vue
+++ Client/src/pages/FrontendPages/Components/Blog/BlogCard.vue
@@ -5,13 +5,13 @@
         <img class="card-img-top" :src="image" :alt="name" />
         <div class="category">{{ categories.length ? categories[categories.length - 1].name : "" }}</div>
         <div class="card-body">
-          <h3 class="card-title text-center pb-2" :class="isLtr ? 'title-ltr' : ''">{{ name }}</h3>
-          <div class="d-flex justify-content-between px-3 blog-info">
-            <span class="blog-info">
+          <h3 class="card-title text-center" :class="isLtr ? 'title-ltr' : ''">{{ name }}</h3>
+          <div class="d-flex justify-content-around card-footer">
+            <span class="card-footer-text">
               {{views}}
               <i class="fa fa-eye"></i>
             </span>
-            <div class="blog-info">
+            <div class="card-footer-text">
               <span class="pl-2">{{ publishedAt }}</span>
               <i class="far fa-calendar-alt"></i>
             </div>
@@ -41,45 +41,46 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.blog-info {
-  color: #646464;
-  font-size: 0.82rem;
+.card .card-footer {
+  background-color: transparent;
+  border-top: 1px solid rgba(158, 158, 158, 0.1);
+  padding: 15px 0 0 0;
+
+  .card-footer-text {
+    color: #919191 !important;
+    font-size: 0.82rem;
+  }
 }
+
 .blogBox {
   border-radius: 10px;
 }
+
 .home-blog .card-img-top {
   border-radius: 10px 10px 0 0;
 }
 
 .home-blog .card {
-  -webkit-box-shadow: 0 0 0 0 #fff;
-  box-shadow: 0 8px 16px 4px rgba(0, 0, 0, 0.12);
-  -webkit-transition: 0.3s;
-  transition: 0.3s;
-  -webkit-transform: translateY(0);
-  transform: translateY(0);
   border: 1px solid rgba(218, 218, 218, 0.582);
-  border-radius: 10px;
+  border-radius: 15px;
   background: transparent;
-}
-.card-body {
-  background: rgb(241, 241, 241);
-  border-radius: 0 0 10px 10px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1), 0 6px 6px rgba(0, 0, 0, 0.1) !important;
 }
 
-.blog-info {
-  background-color: #000285;
-  color: rgba(0, 0, 0, 0.4);
+.card-body {
+  background: rgb(241, 241, 241);
+  border-radius: 0 0 15px 15px;
 }
 
 .home-blog .card:hover {
-  -webkit-box-shadow: 0 10px 30px -5px rgb(107, 107, 107);
-  box-shadow: 0 15px 35px -5px rgba(95, 95, 95, 0.658);
-  -webkit-transform: translateY(-10px);
-  transform: translateY(-10px);
+  -webkit-box-shadow: 0 10px 30px -5px rgba(117, 117, 117, 0.6);
+  box-shadow: 40px rgba(121, 121, 121, 0.6);
+  transition: transform 0.6s;
+  -webkit-transform: translateY(-5px);
+  transform: translateY(-5px);
   cursor: pointer;
 }
+
 .home-blog .card-title {
   color: #292929;
   font-size: 1rem;
@@ -87,7 +88,7 @@ export default {
   line-height: inherit !important;
 
   @media (max-width: 680px) {
-    font-size: 0.9rem;
+    font-size: 1.1em;
   }
 }
 
@@ -100,12 +101,15 @@ export default {
   background-color: #505050 !important;
   font-family: Arial, Helvetica, sans-serif;
 }
+
 .home-blog .card:hover .card-title {
   color: #000285;
 }
+
 .home-blog h3 {
   color: #27408b;
 }
+
 .title-ltr {
   direction: ltr;
 }
