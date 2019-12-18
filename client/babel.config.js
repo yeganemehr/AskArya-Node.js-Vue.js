dd547455afba432554c5a0fbb15390f3cee53d9e
arya107 <arya.doroudian1@gmail.com>
2019-12-18 17:39:19 +0330

    2.3.4.1

--- client/babel.config.js
+++ client/babel.config.js
@@ -2,13 +2,14 @@ module.exports = {
   "presets": [
     "@vue/app"
   ],
-  "plugins": [
-    [
-      "component",
-      {
-        "libraryName": "element-ui",
-        "styleLibraryName": "theme-chalk"
-      }
-    ]
-  ]
+  "plugins": ["@babel/plugin-syntax-dynamic-import"]
+  // "plugins": [
+  //   [
+  //     "component",
+  //     {
+  //       "libraryName": "element-ui",
+  //       "styleLibraryName": "theme-chalk"
+  //     }
+  //   ]
+  // ]
 }
