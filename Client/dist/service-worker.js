ee7772ed25fe24aac27cd5a5762a5f999f5b7b1a
Arya Doroudian <arya.doroudian1@gmail.com>
2019-12-20 17:54:43 +0330

    3.0.0.5

--- Client/dist/service-worker.js
+++ Client/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.493f1e3ba9edd8769e4fe703497e7189.js"
+  "/precache-manifest.323558614653870c0daac5c876fb8319.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "askarya"});
