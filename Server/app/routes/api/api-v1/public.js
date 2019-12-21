aa613a4c6d877b601b95a1ff84d47b7915f4d339
arya107 <arya.doroudian1@gmail.com>
2019-12-21 14:19:05 +0330

    3.0.1.2

--- Server/app/routes/api/api-v1/public.js
+++ Server/app/routes/api/api-v1/public.js
@@ -6,7 +6,7 @@ const CourseController = require('app/http/controllers/api/v1/courseController')
 const HomeController = require('app/http/controllers/api/v1/homeController');
 const AuthController = require('app/http/controllers/api/v1/authController');
 const BlogController = require('app/http/controllers/api/v1/blogController');
-const userController = require('app/http/controllers/api/v1/userController');
+// const userController = require('app/http/controllers/api/v1/userController');
 
 
 // Validator
@@ -34,7 +34,7 @@ router.post(
 );
 
 // Activation
-router.get('/user/activation/:code', userController.activation);
+// router.get('/user/activation/:code', userController.activation);
 
 router.get('/blog/:slug', BlogController.bySlug);
 
