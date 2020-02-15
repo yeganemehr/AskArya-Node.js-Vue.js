42a2768d3b36bbb266a58c0de95327b09d42f773
Arya Doroudian <arya.doroudian1@gmail.com>
2020-02-15 18:58:04 +0330

    3.0.7.1

---
+++ Server/dist/precache-manifest.0b05e09d1a205b61d1c1b1d9fd8a74db.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6cfe2d6439b87deae47",
+    "url": "/css/app.3c1af51e.css"
+  },
+  {
+    "revision": "042c56c13f8546bb653f",
+    "url": "/css/chunk-76d45e16.e6e493b2.css"
+  },
+  {
+    "revision": "1c0e954b83ce15d23452",
+    "url": "/css/chunk-vendors.06384384.css"
+  },
+  {
+    "revision": "589eeb6219cae45af192",
+    "url": "/css/dashboard.21896a01.css"
+  },
+  {
+    "revision": "3f64d9f1b4b9f5bc5bab",
+    "url": "/css/extra.850dd80c.css"
+  },
+  {
+    "revision": "01da5f06d5639ee1d5ed",
+    "url": "/css/pages.959ea23c.css"
+  },
+  {
+    "revision": "94c311fd48c9362dea340aa3a29e3567",
+    "url": "/fonts/IRANSansWeb(FaNum).94c311fd.eot"
+  },
+  {
+    "revision": "bd6f69a8db87710b2f3fcd6ef75bd3e2",
+    "url": "/fonts/IRANSansWeb(FaNum).bd6f69a8.woff"
+  },
+  {
+    "revision": "e9908f05e5771638e40913309b784a17",
+    "url": "/fonts/IRANSansWeb(FaNum).e9908f05.ttf"
+  },
+  {
+    "revision": "eb5adaac0d814e1e8e5cbd75efb9db3e",
+    "url": "/fonts/IRANSansWeb(FaNum).eb5adaac.woff2"
+  },
+  {
+    "revision": "0b5055ac357359f8c23320ea3dc0f78b",
+    "url": "/fonts/IRANSansWeb.0b5055ac.woff2"
+  },
+  {
+    "revision": "66b5cb30e2efe84dab915617acd39041",
+    "url": "/fonts/IRANSansWeb.66b5cb30.woff"
+  },
+  {
+    "revision": "c26a9eb202b3ef6455281f0335cfffe9",
+    "url": "/fonts/IRANSansWeb.c26a9eb2.ttf"
+  },
+  {
+    "revision": "d3931868176480f01fd4524cbd48b667",
+    "url": "/fonts/IRANSansWeb.d3931868.eot"
+  },
+  {
+    "revision": "4abe296380edacb9f146cd778a94f43d",
+    "url": "/fonts/IranSansBold.4abe2963.eot"
+  },
+  {
+    "revision": "753b3827c415580e864a545d1a860a5a",
+    "url": "/fonts/IranSansBold.753b3827.woff"
+  },
+  {
+    "revision": "ceaf6d89af9fb96d0466b26d6f1c022a",
+    "url": "/fonts/IranSansBold.ceaf6d89.woff2"
+  },
+  {
+    "revision": "ff320f78af3a0fd44f2ee2993559fa9f",
+    "url": "/fonts/IranSansBold.ff320f78.ttf"
+  },
+  {
+    "revision": "080752485b7ec8837f9a26cd7c370139",
+    "url": "/fonts/IranSansDN.08075248.eot"
+  },
+  {
+    "revision": "435a590ebb4929cd5b2cbe07e88ae6ca",
+    "url": "/fonts/IranSansDN.435a590e.woff2"
+  },
+  {
+    "revision": "54e2c53a321dd90785d70ba58d68ecdf",
+    "url": "/fonts/IranSansDN.54e2c53a.woff"
+  },
+  {
+    "revision": "72d6571ac2c9cd4e2c95103595454e1b",
+    "url": "/fonts/IranSansDN.72d6571a.ttf"
+  },
+  {
+    "revision": "64e033f99f8b95953df125f88cee8268",
+    "url": "/fonts/IranSansDNBold.64e033f9.ttf"
+  },
+  {
+    "revision": "82f159883cc68ad66bb7392ac20e4045",
+    "url": "/fonts/IranSansDNBold.82f15988.woff2"
+  },
+  {
+    "revision": "90fddb477dc8cd322826f4eed0719d49",
+    "url": "/fonts/IranSansDNBold.90fddb47.woff"
+  },
+  {
+    "revision": "e8165f7a388318a4b0da79c8f3d2b2fd",
+    "url": "/fonts/IranSansDNBold.e8165f7a.eot"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/fonts/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/fonts/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/fonts/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/fonts/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "fb00e5e4e80b6c36816fb4d7d6f9d5d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6cfe2d6439b87deae47",
+    "url": "/js/app-legacy.87587a2b.js"
+  },
+  {
+    "revision": "042c56c13f8546bb653f",
+    "url": "/js/chunk-76d45e16-legacy.19aa681d.js"
+  },
+  {
+    "revision": "1c0e954b83ce15d23452",
+    "url": "/js/chunk-vendors-legacy.8fe9bad3.js"
+  },
+  {
+    "revision": "589eeb6219cae45af192",
+    "url": "/js/dashboard-legacy.a160c25b.js"
+  },
+  {
+    "revision": "3f64d9f1b4b9f5bc5bab",
+    "url": "/js/extra-legacy.065c9626.js"
+  },
+  {
+    "revision": "01da5f06d5639ee1d5ed",
+    "url": "/js/pages-legacy.f1f1f2f8.js"
+  },
+  {
+    "revision": "fc7570b76cf8c00777d57ea731cb00c3",
+    "url": "/manifest.json"
+  }
+]);
