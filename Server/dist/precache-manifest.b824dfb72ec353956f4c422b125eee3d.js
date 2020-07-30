049fa034497f366ce23188cfa1132b18ce637611
Arya Doroudian <arya.doroudian1@gmail.com>
2020-07-30 10:58:54 +0430

    3.1.0.80 - Arya

---
+++ Server/dist/precache-manifest.b824dfb72ec353956f4c422b125eee3d.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b6e9f8072a744804c36",
+    "url": "/css/alltickets.2fdbceda.css"
+  },
+  {
+    "revision": "d77a9dffe9b83ae6e62d",
+    "url": "/css/app.dd7ebf90.css"
+  },
+  {
+    "revision": "a6dfb9db25f378645eeb",
+    "url": "/css/home.83a69537.css"
+  },
+  {
+    "revision": "81f718e8a655cd119364",
+    "url": "/css/quizzes.b60c62bb.css"
+  },
+  {
+    "revision": "7e79a262efda9265b7cf",
+    "url": "/css/resetpassword.72c53960.css"
+  },
+  {
+    "revision": "8f4646ea463db222d860",
+    "url": "/css/terms.18afe2b6.css"
+  },
+  {
+    "revision": "94c311fd48c9362dea340aa3a29e3567",
+    "url": "/fonts/IRANSansWeb(FaNum).94c311fd.eot"
+  },
+  {
+    "revision": "bd6f69a8db87710b2f3fcd6ef75bd3e2",
+    "url": "/fonts/IRANSansWeb(FaNum).bd6f69a8.woff"
+  },
+  {
+    "revision": "e9908f05e5771638e40913309b784a17",
+    "url": "/fonts/IRANSansWeb(FaNum).e9908f05.ttf"
+  },
+  {
+    "revision": "66b5cb30e2efe84dab915617acd39041",
+    "url": "/fonts/IRANSansWeb.66b5cb30.woff"
+  },
+  {
+    "revision": "c26a9eb202b3ef6455281f0335cfffe9",
+    "url": "/fonts/IRANSansWeb.c26a9eb2.ttf"
+  },
+  {
+    "revision": "d3931868176480f01fd4524cbd48b667",
+    "url": "/fonts/IRANSansWeb.d3931868.eot"
+  },
+  {
+    "revision": "4abe296380edacb9f146cd778a94f43d",
+    "url": "/fonts/IranSansBold.4abe2963.eot"
+  },
+  {
+    "revision": "753b3827c415580e864a545d1a860a5a",
+    "url": "/fonts/IranSansBold.753b3827.woff"
+  },
+  {
+    "revision": "ff320f78af3a0fd44f2ee2993559fa9f",
+    "url": "/fonts/IranSansBold.ff320f78.ttf"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/fonts/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/fonts/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/fonts/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "d89acb03a3ca7b485abf313c088f9376",
+    "url": "/img/GB.png"
+  },
+  {
+    "revision": "75c72e84ea1a447250d41d43e9b87ac7",
+    "url": "/img/asc.gif"
+  },
+  {
+    "revision": "d18c690e2896e43c868ee223dd08e36d",
+    "url": "/img/askarya-logo.png"
+  },
+  {
+    "revision": "e6a4e02237dbdfa217ab05645a2aa130",
+    "url": "/img/bg.gif"
+  },
+  {
+    "revision": "5b83f5d5455d6be791088f8bd7d94c01",
+    "url": "/img/default-avatar.png"
+  },
+  {
+    "revision": "5a382f972d79b093248c6b99649d08a7",
+    "url": "/img/desc.gif"
+  },
+  {
+    "revision": "8f171737d320bd81884208072c6ae723",
+    "url": "/img/frontend/SVG-Background-4.svg"
+  },
+  {
+    "revision": "1bfbfc4112a365a70fb96b8b84141ec5",
+    "url": "/img/frontend/aryapilot-askarya.png"
+  },
+  {
+    "revision": "e2a38082987efd926dfaffb3d2d3b341",
+    "url": "/img/frontend/askaryaunderground.svg"
+  },
+  {
+    "revision": "d4d963b6edd8e049a480ba81cc078e0d",
+    "url": "/img/frontend/confused-banner.svg"
+  },
+  {
+    "revision": "8a951ebacf53e659413746d7c78450ab",
+    "url": "/img/frontend/hero-bg-opt.png"
+  },
+  {
+    "revision": "e93e7ed08e3596a475e59c192b4e9250",
+    "url": "/img/frontend/loginscreenbg.svg"
+  },
+  {
+    "revision": "e85be560a13a8bccc417c9261ddfe88a",
+    "url": "/img/frontend/success-banner.svg"
+  },
+  {
+    "revision": "ffd84c778104d3fc9d70da2d1db472ef",
+    "url": "/img/mstile-150x150.png"
+  },
+  {
+    "revision": "a4700898cbe2a716cf567f2159b18511",
+    "url": "/img/placeholder.jpg"
+  },
+  {
+    "revision": "0855b3bb8d00b971a83f75764e199304",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b6e9f8072a744804c36",
+    "url": "/js/alltickets.b1976dc6.js"
+  },
+  {
+    "revision": "d77a9dffe9b83ae6e62d",
+    "url": "/js/app.250ff810.js"
+  },
+  {
+    "revision": "a6dfb9db25f378645eeb",
+    "url": "/js/home.5c92dcaa.js"
+  },
+  {
+    "revision": "81f718e8a655cd119364",
+    "url": "/js/quizzes.174db51a.js"
+  },
+  {
+    "revision": "7e79a262efda9265b7cf",
+    "url": "/js/resetpassword.efcdfb2e.js"
+  },
+  {
+    "revision": "8f4646ea463db222d860",
+    "url": "/js/terms.4336eb1d.js"
+  },
+  {
+    "revision": "ec2ae59d17e5baafac5cffaec6c4ee2b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
--- Server/dist/service-worker.js
+++ Server/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.52996cf17a5ec5c0cbdd570899538c06.js"
+  "/precache-manifest.b824dfb72ec353956f4c422b125eee3d.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "askarya"});
