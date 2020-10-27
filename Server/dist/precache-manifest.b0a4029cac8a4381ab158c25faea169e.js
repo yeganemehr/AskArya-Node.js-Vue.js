1a9bfe70b2d0a54ff57659e25e68d8e9fb6c297f
Arya Doroudian <arya.doroudian1@gmail.com>
2020-10-27 12:23:42 +0330

    6.0.4

---
+++ Server/dist/precache-manifest.b0a4029cac8a4381ab158c25faea169e.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2467aa2f0792fe516972",
+    "url": "/css/alltickets.91f9f47a.css"
+  },
+  {
+    "revision": "203496cf14210ec66a5a",
+    "url": "/css/app.1f6e872e.css"
+  },
+  {
+    "revision": "b310b8e0b25bb1f691cc",
+    "url": "/css/contact.5bbdfabc.css"
+  },
+  {
+    "revision": "a9f6477a4ba138aa82dd",
+    "url": "/css/home.3a7a9467.css"
+  },
+  {
+    "revision": "d1e79e277013e5d4efe9",
+    "url": "/css/quizzes.863365f7.css"
+  },
+  {
+    "revision": "80fee13c4ce382a8a3af",
+    "url": "/css/terms.5ba0d232.css"
+  },
+  {
+    "revision": "94c311fd48c9362dea340aa3a29e3567",
+    "url": "/fonts/IRANSansWeb(FaNum).94c311fd.eot"
+  },
+  {
+    "revision": "bd6f69a8db87710b2f3fcd6ef75bd3e2",
+    "url": "/fonts/IRANSansWeb(FaNum).bd6f69a8.woff"
+  },
+  {
+    "revision": "e9908f05e5771638e40913309b784a17",
+    "url": "/fonts/IRANSansWeb(FaNum).e9908f05.ttf"
+  },
+  {
+    "revision": "66b5cb30e2efe84dab915617acd39041",
+    "url": "/fonts/IRANSansWeb.66b5cb30.woff"
+  },
+  {
+    "revision": "c26a9eb202b3ef6455281f0335cfffe9",
+    "url": "/fonts/IRANSansWeb.c26a9eb2.ttf"
+  },
+  {
+    "revision": "d3931868176480f01fd4524cbd48b667",
+    "url": "/fonts/IRANSansWeb.d3931868.eot"
+  },
+  {
+    "revision": "4abe296380edacb9f146cd778a94f43d",
+    "url": "/fonts/IranSansBold.4abe2963.eot"
+  },
+  {
+    "revision": "753b3827c415580e864a545d1a860a5a",
+    "url": "/fonts/IranSansBold.753b3827.woff"
+  },
+  {
+    "revision": "ff320f78af3a0fd44f2ee2993559fa9f",
+    "url": "/fonts/IranSansBold.ff320f78.ttf"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/fonts/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/fonts/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/fonts/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "d89acb03a3ca7b485abf313c088f9376",
+    "url": "/img/GB.png"
+  },
+  {
+    "revision": "75c72e84ea1a447250d41d43e9b87ac7",
+    "url": "/img/asc.gif"
+  },
+  {
+    "revision": "d18c690e2896e43c868ee223dd08e36d",
+    "url": "/img/askarya-logo.png"
+  },
+  {
+    "revision": "e6a4e02237dbdfa217ab05645a2aa130",
+    "url": "/img/bg.gif"
+  },
+  {
+    "revision": "5b83f5d5455d6be791088f8bd7d94c01",
+    "url": "/img/default-avatar.png"
+  },
+  {
+    "revision": "5a382f972d79b093248c6b99649d08a7",
+    "url": "/img/desc.gif"
+  },
+  {
+    "revision": "5e4acb164b41a2eba13902a33adb82a2",
+    "url": "/img/frontend/SVG-Background-4.svg"
+  },
+  {
+    "revision": "1bfbfc4112a365a70fb96b8b84141ec5",
+    "url": "/img/frontend/aryapilot-askarya.png"
+  },
+  {
+    "revision": "c22c2743227b2c9d1196df580d019888",
+    "url": "/img/frontend/askaryaunderground.svg"
+  },
+  {
+    "revision": "40e85ef916073de8d85761d05c10c1d0",
+    "url": "/img/frontend/confused-banner.svg"
+  },
+  {
+    "revision": "8a951ebacf53e659413746d7c78450ab",
+    "url": "/img/frontend/hero-bg-opt.png"
+  },
+  {
+    "revision": "e93e7ed08e3596a475e59c192b4e9250",
+    "url": "/img/frontend/loginscreenbg.svg"
+  },
+  {
+    "revision": "536a8c6f6564ff4eda00e22bfcf0cac9",
+    "url": "/img/frontend/success-banner.svg"
+  },
+  {
+    "revision": "ffd84c778104d3fc9d70da2d1db472ef",
+    "url": "/img/mstile-150x150.png"
+  },
+  {
+    "revision": "a4700898cbe2a716cf567f2159b18511",
+    "url": "/img/placeholder.jpg"
+  },
+  {
+    "revision": "1ece97ff4bd636258ddf8136a0509ecc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2467aa2f0792fe516972",
+    "url": "/js/alltickets.c972e479.js"
+  },
+  {
+    "revision": "203496cf14210ec66a5a",
+    "url": "/js/app.bdef658d.js"
+  },
+  {
+    "revision": "b310b8e0b25bb1f691cc",
+    "url": "/js/contact.c999a16e.js"
+  },
+  {
+    "revision": "a9f6477a4ba138aa82dd",
+    "url": "/js/home.4ab52133.js"
+  },
+  {
+    "revision": "d1e79e277013e5d4efe9",
+    "url": "/js/quizzes.1d6d3aad.js"
+  },
+  {
+    "revision": "80fee13c4ce382a8a3af",
+    "url": "/js/terms.cba29e35.js"
+  },
+  {
+    "revision": "574a008e768f2bd3b361e4080679b986",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
